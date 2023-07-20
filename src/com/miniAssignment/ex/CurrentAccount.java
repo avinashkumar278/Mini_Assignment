31ec9ce3f5189beeb85ad36b68f0d5426b74c9e6
avinashkumar278 <avinashkumar278@deloitte.com>
2023-07-20 20:18:45 +0530

    Created CurrentAccount class and it's Implementing  bank Interface having deposit, withdraw and interestCalculation methods

---
+++ src/com/miniAssignment/ex/CurrentAccount.java
@@ -0,0 +1,19 @@
+package com.miniAssignment.ex;
+
+public class CurrentAccount implements Bank{
+
+    @Override
+    public void deposit(double amount) {
+
+    }
+
+    @Override
+    public void withdraw(double amount) {
+
+    }
+
+    @Override
+    public void interestCalculation() {
+
+    }
+}
