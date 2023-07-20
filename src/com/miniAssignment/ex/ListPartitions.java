d19dad174cfa4b747cd08b5af973bd8aef66d81b
avinashkumar278 <avinashkumar278@deloitte.com>
2023-07-20 12:21:15 +0530

    updated the print statements for ListPartitions code and added the code description

--- src/com/miniAssignment/ex/ListPartitions.java
+++ src/com/miniAssignment/ex/ListPartitions.java
@@ -1,3 +1,9 @@
+/*
+4.	Write a program to break a list into n size batches of list
+I/P: [1,2,3,5,6,7,8,9,10,4,11]
+n=3
+O/P: [[1,2,3],[5,6,7],[8,9,10],[4,11]]
+ */
 package com.miniAssignment.ex;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -7,10 +13,10 @@ import java.util.List;
         public static void main(String[] args) {
             int[] arrayInput = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
             int batchSize = 3;
-
+            System.out.println("ListPartitions : ");
             List<int[]> batches = createBatches(arrayInput, batchSize);
             for (int i = 0; i < batches.size(); i++) {
-                System.out.println(Arrays.toString(batches.get(i)));
+                System.out.print(Arrays.toString(batches.get(i)));
             }
         }
         public static List<int[]> createBatches(int[] array, int batchSize) {
