ca2e870fa47fcd9250ab4a775e6c25c2244a75ea
avinashkumar278 <avinashkumar278@deloitte.com>
2023-07-20 21:27:19 +0530

    implements the deposit(), withdraw(), interestCalculation() in SavingsAccount class

--- src/com/miniAssignment/ex/SavingsAccount.java
+++ src/com/miniAssignment/ex/SavingsAccount.java
@@ -1,18 +1,41 @@
 package com.miniAssignment.ex;
 
 public class SavingsAccount implements Bank{
+    private double balance;
+    private double interestRate;
+
+    public SavingsAccount(double initialBalance, double interestRate) {
+        this.balance = initialBalance;
+        this.interestRate = interestRate;
+    }
+
     @Override
     public void deposit(double amount) {
-
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: $" + amount);
+            System.out.println("Current Balance: $" + balance);
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
     }
 
     @Override
     public void withdraw(double amount) {
-
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: $" + amount);
+            System.out.println("Current Balance: $" + balance);
+        } else {
+            System.out.println("Invalid withdrawal amount or insufficient balance.");
+        }
 
     }
     @Override
     public void interestCalculation() {
-
+        double interestAmount = balance * interestRate / 100;
+        balance += interestAmount;
+        System.out.println("Interest Calculated: $" + interestAmount);
+        System.out.println("Current Balance (After Interest): $" + balance);
     }
 }
