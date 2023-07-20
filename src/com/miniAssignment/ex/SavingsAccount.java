d7ae233f35ffbe1e2662ebe14c7c855504a4424a
avinashkumar278 <avinashkumar278@deloitte.com>
2023-07-20 20:14:06 +0530

    Created SavingsAccount class and it's Implementing  bank Interface having deposit, withdraw and interestCalculation methods

---
+++ src/com/miniAssignment/ex/SavingsAccount.java
@@ -0,0 +1,18 @@
+package com.miniAssignment.ex;
+
+public class SavingsAccount implements Bank{
+    @Override
+    public void deposit(double amount) {
+
+    }
+
+    @Override
+    public void withdraw(double amount) {
+
+
+    }
+    @Override
+    public void interestCalculation() {
+
+    }
+}
