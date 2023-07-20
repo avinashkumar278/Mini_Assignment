9b337f53470ffe274a7f2b5f586d42342470f59d
avinashkumar278 <avinashkumar278@deloitte.com>
2023-07-20 22:30:06 +0530

    implements the deposit(), withdraw(), interestCalculation() in CurrentAccount class

--- src/com/miniAssignment/ex/CurrentAccount.java
+++ src/com/miniAssignment/ex/CurrentAccount.java
@@ -1,19 +1,38 @@
 package com.miniAssignment.ex;
 
 public class CurrentAccount implements Bank{
+    private double balance;
+    private double overdraftLimit;
 
+    public CurrentAccount(double initialBalance, double overdraftLimit) {
+        this.balance = initialBalance;
+        this.overdraftLimit = overdraftLimit;
+    }
     @Override
     public void deposit(double amount) {
-
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: $" + amount);
+            System.out.println("Current Balance: $" + balance);
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
     }
 
     @Override
     public void withdraw(double amount) {
-
+        if (amount > 0 && amount <= (balance + overdraftLimit)) {
+            balance -= amount;
+            System.out.println("Withdrawn: $" + amount);
+            System.out.println("Current Balance: $" + balance);
+        } else {
+            System.out.println("Invalid withdrawal amount or exceeds overdraft limit.");
+        }
     }
 
     @Override
     public void interestCalculation() {
-
+        // Current accounts typically do not earn interest
+        System.out.println("Interest is not applicable to Current Account.");
     }
 }
